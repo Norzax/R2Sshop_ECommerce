ca2f6a8b4a0de435a561180d74029e415e79a76a
giang1911 <01666543494giang@gmail.com>
2023-12-15 19:29:03 +0700

    feature/giang/category-service-repository-controller

--- src/main/java/com/aclass/r2sshop_ecommerce/services/category/CategoryService.java
+++ src/main/java/com/aclass/r2sshop_ecommerce/services/category/CategoryService.java
@@ -1,6 +1,11 @@
 package com.aclass.r2sshop_ecommerce.services.category;
 
+import com.aclass.r2sshop_ecommerce.models.dto.CategoryDTO;
+import com.aclass.r2sshop_ecommerce.models.dto.common.ResponseDTO;
 import com.aclass.r2sshop_ecommerce.services.Service;
 
+import java.util.List;
+
 public interface CategoryService extends Service {
+    ResponseDTO<List<CategoryDTO>> findAll();
 }
