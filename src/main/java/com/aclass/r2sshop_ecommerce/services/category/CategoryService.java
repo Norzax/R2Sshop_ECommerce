84989153f372e7e11be4f23d58320848637b8f76
giang1911 <01666543494giang@gmail.com>
2023-12-16 15:56:19 +0700

    feature/giang/update-category

--- src/main/java/com/aclass/r2sshop_ecommerce/services/category/CategoryService.java
+++ src/main/java/com/aclass/r2sshop_ecommerce/services/category/CategoryService.java
@@ -2,10 +2,17 @@ package com.aclass.r2sshop_ecommerce.services.category;
 
 import com.aclass.r2sshop_ecommerce.models.dto.CategoryDTO;
 import com.aclass.r2sshop_ecommerce.models.dto.common.ResponseDTO;
+import com.aclass.r2sshop_ecommerce.models.entity.CategoryEntity;
 import com.aclass.r2sshop_ecommerce.services.Service;
 
 import java.util.List;
 
-public interface CategoryService extends Service {
+public interface CategoryService extends Service<CategoryDTO> {
+
     ResponseDTO<List<CategoryDTO>> findAll();
+    ResponseDTO<CategoryDTO> findById(Long id);
+    ResponseDTO<CategoryDTO> create(CategoryDTO categoryDTO);
+    ResponseDTO<CategoryDTO> update(Long id, CategoryDTO categoryDTO);
+    ResponseDTO<Void> delete(Long id);
+
 }
