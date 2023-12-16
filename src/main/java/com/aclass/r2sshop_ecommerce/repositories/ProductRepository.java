52aa688ae6396ff4b31fab2ae6b24ecc6cd95146
giang1911 <01666543494giang@gmail.com>
2023-12-16 17:50:22 +0700

    feature/giang/product

--- src/main/java/com/aclass/r2sshop_ecommerce/repositories/ProductRepository.java
+++ src/main/java/com/aclass/r2sshop_ecommerce/repositories/ProductRepository.java
@@ -1,4 +1,21 @@
 package com.aclass.r2sshop_ecommerce.repositories;
 
-public interface ProductRepository {
+import com.aclass.r2sshop_ecommerce.models.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+    @Query("SELECT COUNT(p) FROM ProductEntity p WHERE p.category.id = :categoryId")
+    int countProductsByCategoryId(@Param("categoryId") Long categoryId);
+
+    @Query("SELECT p FROM ProductEntity p WHERE p.category.id = :categoryId")
+    List<ProductEntity> findProductsByCategoryIdWithPaging(
+            @Param("categoryId") Long categoryId,
+            int startIndex,
+            int pageSize
+    );
 }
