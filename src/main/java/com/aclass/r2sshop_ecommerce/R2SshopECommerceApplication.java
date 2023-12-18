70db23014767b7cd747cfea76c40830435212be9
Giang Bảo Luân <giangbaoluan5@gmail.com>
2023-12-19 06:23:00 +0700

    feature/baoluan/them swagger

--- src/main/java/com/aclass/r2sshop_ecommerce/R2SshopECommerceApplication.java
+++ src/main/java/com/aclass/r2sshop_ecommerce/R2SshopECommerceApplication.java
@@ -2,9 +2,8 @@ package com.aclass.r2sshop_ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication (exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class R2SshopECommerceApplication {
 
     public static void main(String[] args) {
